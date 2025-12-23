f36fbff3b76e26bd9662fa1a9651b9f0be949277
keihanikakauLOL <arm.nantana@gmail.com>
2025-12-23 15:19:57 +0700

    nicee

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,11 +6,17 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
+	string txt;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	dest << "-------------------- BOOM ---------------------" << "\n";
+	while (getline(source, txt) ) {
+		dest << txt << "\n";
+	}
+	dest << "-------------------- HA!! ---------------------" << "\n";
 
     source.close();
     dest.close();
